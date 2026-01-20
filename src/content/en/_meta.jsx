9886e6d4319bbca6ac949e31ecbcc870f5c1aa0b
ayimany <ayimany.dev@proton.me>
2026-01-20 16:20:38 -0600

    Add main homepage index

---
+++ src/content/en/_meta.jsx
@@ -0,0 +1,14 @@
+export default {
+    index: {
+        title: 'Home',
+        type: 'page',
+    },
+    about: {
+        title: 'About FXST',
+        type: 'page',
+    },
+    manufacturing: {
+        title: 'Manufacturing',
+        type: 'page',
+    },
+}
---
+++ src/content/es/_meta.jsx
@@ -0,0 +1,14 @@
+export default {
+    index: {
+        title: 'Inicio',
+        type: 'page',
+    },
+    about: {
+        title: 'Acerca de FXST',
+        type: 'page',
+    },
+    manufacturing: {
+        title: 'Manufactura',
+        type: 'page',
+    },
+}
